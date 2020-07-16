0573249c575bc108e8690ed04ed9ad787ed8efd4
Charlie Barth <charliepbarth@gmail.com>
2020-07-16 20:44:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
   attr_reader :songs
   
   extend Memorable::ClassMethods
-
+  include Paramable
 
   @@all = []
 
@@ -30,10 +30,6 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
   
 end
